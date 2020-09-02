815a26bec18e61e7127e3114d5940027b1d61496
Jeff Johnston <jeffwjohn@yahoo.com>
2020-09-02 15:13:58 -0500

    complete citySearch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,21 @@
 // .then(response => response.json())
 // .then(data => console.log(data));
 
-var citySearch = function () {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037")
+var citySearch = function (city) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=43fd1cdd770e5cf56daf2f9d5cdc1037";
+    console.log(apiUrl);
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            
+        })
+       
+    });
+    
 };
 
-citySearch();
+citySearch('Austin');
+
 
 // http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037
