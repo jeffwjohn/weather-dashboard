ef8c0c08f9fa5d0d4e24d9e2de1a4f06307ff652
Jeff Johnston <jeffwjohn@yahoo.com>
2020-09-02 14:51:47 -0500

    add fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
-.then(response => response.json())
-.then(data => console.log(data));
+// fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+// fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+var citySearch = function () {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037")
+};
+
+citySearch();
+
+// http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037
