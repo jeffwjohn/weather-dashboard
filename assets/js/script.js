19343261b9d593d8fa035181799d4e08d8292985
Jeff Johnston <jeffwjohn@yahoo.com>
2020-09-05 15:20:40 -0500

    clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,7 +165,7 @@ var displayFiveDay = function (data) {
     for (var i = 0; i < data.length; i++) {
 
         var day = document.getElementById("day" + i);
-        day.innerHTML = '<h4 class="card-title">' + data[i].date + '</h4><img id="icon' + i + '"class="col-10" alt="weather-conditions-icon" src="https://openweathermap.org/img/wn/' + data[i].icon + '@2x.png"></img><p>Temp: ' + data[i].temp + ' ℉</p><p>Humidity: ' + data[i].humidity + '%</p>';
+        day.innerHTML = '<h6 class="card-title">' + data[i].date + '</h6><img id="icon' + i + '"class="col-10" src="https://openweathermap.org/img/wn/' + data[i].icon + '@2x.png"></img><p>Temp: ' + data[i].temp + ' ℉</p><p>Humidity: ' + data[i].humidity + '%</p>';
 
     }
     return
